e639f2b4d75c52e809e7ceaf5e9bed3e929d6ec3
chtpr <lee.christophe@northeastern.edu>
2024-01-12 13:45:34 -0800

    added search by genre and new rating field

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce10c90367ac57f0bb992dd5a1cc08a93e5c4970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df792e6b9b61ed1afab38ec6bdb12e2f7af061bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
