34129535f2bfe324c11bb8c806faa3180377a401
chtpr <lee.christophe@northeastern.edu>
2024-01-13 15:31:13 -0800

    added field validation

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df792e6b9b61ed1afab38ec6bdb12e2f7af061bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e639f2b4d75c52e809e7ceaf5e9bed3e929d6ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
